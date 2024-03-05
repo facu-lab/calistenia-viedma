f2112af5dac8d4ad71a08a1342974d76420c04fc
Facundo Muñoz <alexisfacom14@gmail.com>
2024-03-05 13:36:36 -0300

    subir carpeta archive

---
+++ src/archive/fontawesome.js
@@ -0,0 +1,11 @@
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faCircleInfo, faDumbbell, faHeart, faUser, faUsers } from "@fortawesome/free-solid-svg-icons"
+
+
+library.add(
+    faCircleInfo,
+    faDumbbell,
+    faHeart,
+    faUser,
+    faUsers
+)
