bdaf14b5c5b3afab2cbb2f5095751ac37328e983
Facundo Muñoz <alexisfacom14@gmail.com>
2024-03-05 17:14:07 -0300

    logica del menu responsive desplegable

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,19 +3,22 @@ import "../styles/nav.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 import logo from "../assets/iconoViedmaReal.png"
+import React, { useState } from "react"
  
 const barsMenu=<FontAwesomeIcon icon={faBars} size="2xl" style={{color: "#ffffff",}} />;
 
 
 const Nav=()=>{
-  
+
+  const [stateBar, setStateBar]= useState(false);
+
   return(
     <nav>
       <div className="contenedor-logoCV">
         <Link className="logoCV" to='/'><img src={logo} /></Link>
       </div>
-      <div className="barsMenu">{barsMenu}</div>
-      <ul className="menu">
+      <div onClick={()=>{stateBar?setStateBar(false):setStateBar(true)}} className="barsMenu">{barsMenu}</div>
+      <ul className={stateBar?"menu":"menu noMenu"}>
         <li><Link className="linkNav" to='/Entrenamientos'><div className="iconLinkNav"><FontAwesomeIcon icon="fa-solid fa-dumbbell" size="sm" style={{color:"#ffffff"}}/></div>Entrenamientos</Link></li>
         <li><Link className="linkNav" to='/Gymnacios'><div className="iconLinkNav"><FontAwesomeIcon icon="fa-solid fa-heart" size="sm" style={{color:"#ffffff"}}/></div>Gymnacios</Link></li>
         <li><Link className="linkNav" to='/Nosotros'><div className="iconLinkNav"><FontAwesomeIcon icon="fa-solid fa-users" size="sm" style={{color:"#ffffff"}}/></div>Nosotros</Link></li>
